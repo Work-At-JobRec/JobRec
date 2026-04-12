b418ab74dc6bbb1e677e38fc3ccde0425a052149
Silicasandwhich <43558271+Silicasandwhich@users.noreply.github.com>
2026-04-12 17:50:46 -0400

    resume parsing and waiting

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import os
 from flask import Flask, flash, request, redirect, render_template, url_for
 from werkzeug.utils import secure_filename
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import Session
+from openaiapi import UserInfoTable, update_skill_db, UserInfo, Base
+from threading import Thread
 
 
 RESUME_FOLDER = "uploads"
@@ -11,9 +15,24 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = RESUME_FOLDER
 app.secret_key = "test"
 
+mock_userid: bytes = b"team 6"
+
+engine = create_engine("sqlite+pysqlite:///user_skills.db")
+Base.metadata.create_all(engine)
+
+
 @app.route('/')
 def home():
-    return render_template("jobprofile.html")
+    with Session(engine) as session:
+        stmt = select(UserInfoTable).where(UserInfoTable.user_id == mock_userid)
+        try:
+            user_info_raw = session.scalars(stmt).one()
+        except:
+            return render_template("jobprofile.html")
+    if user_info_raw.done_processing:
+        user_info = UserInfo.model_validate_json(user_info_raw.info)
+        return render_template("jobprofile.html", user_info=user_info)
+    return render_template("jobprofile_processing.html")
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -36,6 +55,22 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # add empty user info to db if not already present
+            with Session(engine) as session:
+                stmt = select(UserInfoTable).where(UserInfoTable.user_id == mock_userid)
+                try:
+                    user_info = session.scalars(stmt).one()
+                    user_info.done_processing=False
+                except:
+                    user_info = UserInfoTable(
+                        user_id=mock_userid, info="{}", done_processing=False
+                    )
+                session.add(user_info)
+                session.commit()
+                p = Thread(target=update_skill_db, args=[mock_userid, engine, f"uploads/{filename}"])
+                p.start()
+                print("finished scheduling process")
+                return redirect(url_for('home'))
     return redirect(url_for('home'))
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ openaiapi.py
@@ -0,0 +1,101 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+from typing import Annotated
+from pydantic import BaseModel, Field, HttpUrl
+from sqlalchemy import select
+from sqlalchemy.orm import DeclarativeBase, Session
+import sqlalchemy
+
+_ = load_dotenv()
+
+client = OpenAI()
+
+class Base(DeclarativeBase):
+    pass
+
+class UserInfoTable(Base):
+    __tablename__ = "user_info"
+
+    user_id = sqlalchemy.Column("user_id", sqlalchemy.BLOB, primary_key=True)
+    info = sqlalchemy.Column("skills", sqlalchemy.JSON)
+    done_processing = sqlalchemy.Column("done_processing", sqlalchemy.Boolean)
+
+class SkillRanking(BaseModel):
+    skill_name: str = Field(
+        ...,
+        description="Human-readable and distinct name representing a specific skill",
+    )
+    proficiency_level: int = Field(
+        ...,
+        le=4,
+        ge=1,
+        description="Numerical ranking of an applicant's proficiency in this skill from 1-4, where 1 is basic familiarity, 2 is extensive amateur experience, 3 is professional or academic experience, and 4 is proven, long-term mastery.",
+    )
+
+class Education(BaseModel):
+    school: str = Field(..., description="School that the degree was earned from")
+    degree: str = Field(..., description="Degree level and field of study achieved from this university (e.g. bachelor's in art history). Do not include GPA.")
+
+class Social(BaseModel):
+    platform: str = Field(..., description="Name of platform where this account is hosted")
+    url: str = Field(..., description="Full URL of the user's account on this platform, including hostname")
+
+class Employment(BaseModel):
+    company_name: str = Field(..., description="Name of the company where the user worked")
+    role: str = Field(..., description="User's role at the company")
+
+
+
+class UserInfo(BaseModel):
+    skills: list[SkillRanking] = Field(
+        ..., description="All skills that the applicant has any experience with."
+    )
+    education: list[Education] = Field(..., description="All education experience the applicant has")
+    projects: list[str] = Field(..., description="Names of all projects the user has experience with (do not include other information)")
+    socials: list[Social] = Field(..., description="All personal (not organizational) social media included in the user's resume")
+    employment_history: list[Employment] = Field(..., description="All previous work experience of the user")
+
+
+
+def update_skill_db(user_id: bytes, engine: sqlalchemy.Engine, filename: str):
+    user_info = parse_resume(filename)
+    with Session(engine) as session:
+
+        stmt = select(UserInfoTable).where(UserInfoTable.user_id == user_id)
+        try:
+            skill_ranking = session.scalars(stmt).one()
+        except:
+            return None
+
+        if user_info is None:
+            skill_ranking.done_processing = True
+            return
+        else:
+
+            skill_ranking.info = user_info.model_dump_json()
+            skill_ranking.done_processing = True
+
+        session.commit()
+
+
+def parse_resume(filename: str) -> UserInfo | None:
+
+    uploaded = client.files.create(file=open(filename, "rb"), purpose="assistants")
+    response = client.responses.parse(
+        model="gpt-5.2",
+        instructions="""You are an HR manager who is an expert in reading and parsing resumes.
+        First, determine the user's employment history, project experience, education, and any linked social media presences. 
+        Then, determine what skills the applicant has from their PDF resume. 
+        Furthermore, rank their proficiency in each skill on a scale from 1-4, where 1 is basic familiarity, 2 is extensive amateur experience, 3 is professional or academic experience, and 4 is proven, long-term mastery.""",
+        input=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "input_file", "file_id": uploaded.id},
+                ],
+            }
+        ],
+        text_format=UserInfo,
+    )
+
+    return response.output_parsed
